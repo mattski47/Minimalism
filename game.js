93232dd0bd9a6c96dde68f085c90e6c65df85a68
mattski47 <matthewsski@gmail.com>
2016-05-18 12:45:58 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -1,26 +1,54 @@
 var gameport = document.getElementById("gameport");
 
-var renderer = PIXI.autoDetectRenderer(400, 400, {backgroundColor: 0x3344ee});
+var renderer = PIXI.autoDetectRenderer(400, 400);
 gameport.appendChild(renderer.view);
 
 var stage = new PIXI.Container();
 
-var texture = PIXI.Texture.fromImage("person1.png");
+//get textures
+var backgroundTexture = PIXI.Texture.fromImage("cavebackground.png");
+var playerTexture = PIXI.Texture.fromImage("person1.png");
 
-var sprite = new PIXI.Sprite(texture);
+//create sprites
+var backgroundSprite = new PIXI.Sprite(backgroundTexture);
+var playerSprite = new PIXI.Sprite(playerTexture);
 
-sprite.anchor.x = 0.5;
-sprite.anchor.y = 0.5;
+//place background
+backgroundSprite.anchor.x = 0.5;
+backgroundSprite.anchor.y = 0.5;
+backgroundSprite.position.x = 200;
+backgroundSprite.position.y = 200;
 
-sprite.position.x = 200;
-sprite.position.y = 200;
+//place character
+playerSprite.anchor.x = 0.5;
+playerSprite.anchor.y = 0.5;
+playerSprite.position.x = 200;
+playerSprite.position.y = 200;
 
-stage.addChild(sprite);
+//add sprites to stage
+stage.addChild(backgroundSprite);
+stage.addChild(playerSprite);
 
 function animate() {
 	requestAnimationFrame(animate);
-	sprite.rotation += 0.1;
 	renderer.render(stage);
 }
 
+function keydownEventHandler(e) {
+	if (e.keyCode == 65) { //key a
+		if (playerSprite.position.x != 8) {
+			playerSprite.position.x -= 5;
+		}
+	}
+	
+	if (e.keyCode == 68) { //key d
+		if (playerSprite.position.x != renderer.width-8) {
+			playerSprite.position.x += 5;
+		}
+	}
+	
+	
+}
+
 animate();
+document.addEventListener('keydown', keydownEventHandler);
